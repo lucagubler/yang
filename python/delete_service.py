a8c0349148b87bd4f8244710a06952aa02d0b40a
Jerome Gygax <jerome.gygax@gyj.ch>
2018-12-11 10:12:11 +0100

    add sleep timers to avoid lock errors because that API is that slow.

--- python/delete_service.py
+++ python/delete_service.py
@@ -14,7 +14,8 @@ for device in devices:
     # Remove VRF from BGP
     print('\n===============  Delete VRF Config  ===============\n')
     url = "https://" + device + "/restconf/data/Cisco-IOS-XE-native:native/router/bgp=65000/address-family/with-vrf/ipv4=unicast/vrf=myVRF"
-
+    
+    time.sleep(7)
     response = requests.request("DELETE", url, headers=common_data.headers, verify=False)
 
     common_data.printApiResponse(response)
@@ -22,8 +23,7 @@ for device in devices:
     # Delete VRF itself
     url = "https://" + device + "/restconf/data/Cisco-IOS-XE-native:native/vrf/definition=myVRF"
 
+    time.sleep(7)
     response = requests.request("DELETE", url, headers=common_data.headers, verify=False)
 
     common_data.printApiResponse(response)
-
-
