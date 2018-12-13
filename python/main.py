2f028187d09f7ced2110966a83a9dd8101139047
gyjch <jerome.gygax@gyj.ch>
2018-12-13 19:18:38 +0100

    file housekeeping

--- python/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.system('python list_vrf.py')
-os.system('python deploy_service.py')
-os.system('python delete_service.py')
-os.system('python deploy_bgp_neighbor.py')
